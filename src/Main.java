dc87fc79b1b108d1e379557c61da2b306c5aac17
jdevit <jez_is_the_best@hotmail.com>
2019-08-26 16:10:07 +0100

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Second line");
+
+//        1. Read pdf files
+//        2. Convert pdf to text
+//        3. Split text content into individual words
+//        4. Filter out ascii symbols or punctiation
+//          4.a. Memory-managment memory management (Punctuation management)
+//        5. Get word term frequency
+//        6. Get TF IDF (formula)
+//        7. UI to apply search and print out results
+
+
     }
 }
